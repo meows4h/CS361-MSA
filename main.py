8919ed1446c6d117dbd4ceaa5b31ad075ec08814
rox <124093142+meows4h@users.noreply.github.com>
2024-11-20 21:25:41 -0800

    further adjustments

--- main.py
+++ main.py
@@ -34,28 +34,58 @@ def create_account(username, password):
     try:
         cursor.execute(query)
         connection.commit()
-        return f"Successfully created an account (username: {username}; password: {password})"
+        return f"Created account (username: {username}; password: {password})"
     
     except Error as e:
         return f"The error '{e}' occurred"
     
 # edit an account
-def edit_account(userid, username, password):
+def edit_account(userid, username='', password=''):
     cursor = connection.cursor()
 
-    query = f"""
-    UPDATE
-      `accounts`
-    SET  
-      username = '{username}', password = '{password}'
-    WHERE
-      id = {userid}
-    """
+    query = ''
+    reply = ''
+
+    if username == '':
+        query = f"""
+        UPDATE
+          `accounts`
+        SET  
+          password = '{password}'
+        WHERE
+          id = {userid}
+        """
+
+        reply = f"Edited account (id: {userid}; password: {password})"
+
+    elif password == '':
+        query = f"""
+        UPDATE
+          `accounts`
+        SET  
+          username = '{username}'
+        WHERE
+          id = {userid}
+        """
+
+        reply = f"Edited account (id: {userid}; username: {username})"
+
+    else:
+        query = f"""
+        UPDATE
+          `accounts`
+        SET  
+          username = '{username}', password = '{password}'
+        WHERE
+          id = {userid}
+        """
+
+        reply = f"Edited account (id: {userid}; username: {username}; password: {password})"
 
     try:
         cursor.execute(query)
         connection.commit()
-        return f"Successfully edited an account (id: {userid}; username: {username}; password: {password})"
+        return reply
     
     except Error as e:
         return f"The error '{e}' occurred"
@@ -74,33 +104,33 @@ def delete_account(userid):
     try:
         cursor.execute(query)
         connection.commit()
-        return f"Successfully deleted an account (id: {userid})"
+        return f"Deleted account (id: {userid})"
     
     except Error as e:
         return f"The error '{e}' occurred"
     
 # search & validate an account
 def search_account(username, password):
-
-def execute_query(connection, query):
     cursor = connection.cursor()
+
+    query = f"""
+    SELECT
+      'id'
+    FROM
+      `accounts`
+    WHERE
+      username = '{username}'
+    AND
+      password = '{password}'
+    """
+
     try:
         cursor.execute(query)
         connection.commit()
-        print("Query executed successfully")
+        return f"Found account with id: "
+    
     except Error as e:
-        print(f"The error '{e}' occurred")
-
-create_users = """
-INSERT INTO
-  `users` (`name`, `age`, `gender`, `nationality`)
-VALUES
-  ('James', 25, 'male', 'USA'),
-  ('Leila', 32, 'female', 'France'),
-  ('Brigitte', 35, 'female', 'England'),
-  ('Mike', 40, 'male', 'Denmark'),
-  ('Elizabeth', 21, 'female', 'Canada');
-"""
+        return f"The error '{e}' occurred"
 
 # zeromq
 context = zmq.Context()
@@ -115,39 +145,60 @@ while True:
     message = message.decode('utf-8')
     print(f'Received request: "{message}"')
 
+    # create & search
     message_arr = message.split(' ')
-    if len(message_arr) >= 3:
+    if len(message_arr) == 3:
         request = message_arr[0]
         username = message_arr[1]
         password = message_arr[2]
 
         # ex: 'create [username] [password]'
-        # return if available: '201 Created: [username] [password] [id]'
-        # return if not available: '409 Conflict: [username] not available'
         if request == 'create':
-
-            account_check = False # if true: username taken
-
-            if account_check:
-                reply = f'409 Conflict: {username} not available'
-            else:
-                reply = f'201 Created: {username} {password}'
-
-        # ex: 'delete [username] [password]'
-        # return if found: '200 OK: Deleted [username] [id]'
-        # return if not found: '404 Not Found: [username]'
-        elif request == 'delete':
-
-            reply = f'200 OK: Deleted {username}'
-            reply = f'404 Not Found: {username}'
+          
+            reply = create_account(username, password)
 
         # ex: 'search [username] [password]'
-        # return if found: '200 OK: [id]'
-        # return if not found: '404 Not Found: [username] [password]'
         elif request == 'search':
 
-            reply = f'200 OK: [id]'
-            reply = f'404 Not Found: {username} {password}'
+            reply = search_account(username, password)
+
+    # delete
+    elif len(message_arr) == 2:
+        request = message_arr[0]
+        userid = message_arr[1]
+
+        # ex: 'delete [userid]'
+        if request == 'delete':
+
+            reply = delete_account(userid)
+
+    # edit
+    elif len(message_arr) >= 4:
+        request = message_arr[0]
+        parameter = message_arr[1]
+        userid = message_arr[2]
+        first_input = message_arr[3]
+
+        if len(message_arr) >= 5:
+            second_input = message_arr[4]
+
+        # ex: 'edit [param] [userid] ...'
+        if request == 'edit':
+
+            # ex: 'edit username [userid] [username]'
+            if parameter == 'username':
+
+                reply = edit_account(userid, username=first_input)
+
+            # ex: 'edit username [userid] [password]'
+            elif parameter == 'password':
+
+                reply = edit_account(userid, password=first_input)
+
+            # ex: 'edit both [userid] [username] [password]'
+            elif parameter == 'both':
+
+                reply = edit_account(userid, first_input, second_input)
 
     # ex: missing components or not enough queries, or other problems
     # return: '400 Bad Request'
---
+++ test.py
@@ -0,0 +1,3 @@
+import zmq
+
+# sender file to test call back
